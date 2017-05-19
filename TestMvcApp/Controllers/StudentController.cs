ac6095895a240c80decdf49c45204d0751dadf20
Faisal Ghaffar <faisalg92@gmail.com>
2017-05-19 14:40:59 +0500

    changes

---
+++ TestMvcApp/Controllers/StudentController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TestMvcApp.Models;
+
+namespace TestMvcApp.Controllers
+{
+    public class StudentController : ApiController
+    {
+        public List<StudentModel> Get()
+        {
+            var list =  new List<StudentModel>();
+            list.Add(new StudentModel
+            {
+                RollNumber="BSEF14M041",
+                StudentName="Muzna Aleem"
+            });
+            list.Add(new StudentModel
+            {
+                RollNumber = "BSEF09M008",
+                StudentName = "Faisal Ghaffar"
+            });
+
+            return list;
+        }
+    }
+}
---
+++ TestMvcApp/Models/StudentModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestMvcApp.Models
+{
+    public class StudentModel
+    {
+        public string RollNumber { get; set; }
+        public string StudentName { get; set; }
+    }
+}
